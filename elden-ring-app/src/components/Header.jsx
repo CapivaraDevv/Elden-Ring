151084e4c9eb17acc82f40d3a9279d6cefc60b5f
Pedro <pedro.henrique.pereira.2014@gmail.com>
2026-03-25 15:26:33 -0300

    Alterações no design de titulos e textos

--- elden-ring-app/src/components/Header.jsx
+++ elden-ring-app/src/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header() {
     return (
         <>
-            <div className="min-w-screen justify-center bg-black grid grid-cols-2 grid-flow-row p-6">
-                <div className="px-8">
-                    <h1 className="text-2xl tracking-wider text-yellow-500 logo">Elden Ring</h1>
+            <div className="min-w-screen justify-center grid grid-cols-2 grid-flow-row p-6">
+                <div className="px-8 flex">
+                    <h1 className="text-2xl tracking-wider text-yellow-200 logo transition-all cursor-pointer">Elden Ring</h1>
                 </div>
                 <div className="">
                     
--- elden-ring-app/src/pages/Home.jsx
+++ elden-ring-app/src/pages/Home.jsx
@@ -3,9 +3,9 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-scree text-white">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Elden Ring App</h1>
+      <h1 className="text-3xl mb-4 hero-title flex">Bem-vindo as terras intermédias</h1>
       <p className="text-lg mb-8">Explore the world of Elden Ring and discover its secrets.</p>
-      <button className="px-6 py-3 bg-blue-600 rounded hover:bg-blue-700 transition duration-300">
+      <button className="px-6 py-3 logo tracking-wider font-bold bg-transparent rounded hover:bg-yellow-400 transition duration-300">
         Get Started
       </button>
     </div>
