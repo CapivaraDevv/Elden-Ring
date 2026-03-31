10b37c0385a6eb2ef684c334aa8afede96de452d
Pedro <pedro.henrique.pereira.2014@gmail.com>
2026-03-31 08:54:26 -0300

    Melhoria no hover da navbar

--- elden-ring-app/src/components/Header.jsx
+++ elden-ring-app/src/components/Header.jsx
@@ -8,29 +8,29 @@ export default function Header() {
           </h1>
         </div>
         <div className="text-text-dim logo flex gap-12">
-          <div className="relative group">
-            <h2 className="nav-links hover:text-gold-light cursor-pointer transition-all">
+          <div className="relative">
+            <h2 className="nav-links hover:text-gold-light cursor-pointer transition-all group">
               Lore
+              <div className="absolute left-0 -bottom-1  w-0 h-[1px] bg-yellow-300 transition-all duration-300 group-hover:w-full"></div>
             </h2>
-            <div className="absolute left-0 -bottom-1  w-0 h-[1px] bg-yellow-300 transition-all duration-300 group-hover:w-full"></div>
           </div>
-          <div className="relative group">
-            <h2 className="nav-links hover:text-gold-light cursor-pointer transition-all">
+          <div className="relative">
+            <h2 className="nav-links hover:text-gold-light cursor-pointer transition-all group">
               Locais
+              <div className="absolute left-0 -bottom-[1px] w-0 h-[1px] bg-yellow-300 transition-all duration-300 group-hover:w-full"></div>
             </h2>
-            <div className="absolute left-0 -bottom-[1px] w-0 h-[1px] bg-yellow-300 transition-all duration-300 group-hover:w-full"></div>
           </div>
-          <div className="relative group">
-            <h2 className="nav-links hover:text-gold-light cursor-pointer transition-all">
+          <div className="relative">
+            <h2 className="nav-links hover:text-gold-light cursor-pointer transition-all group">
               Personagens
+              <div className="absolute left-0 -bottom-1 w-0 h-[1px] bg-yellow-300 transition-all duration-300 group-hover:w-full"></div>
             </h2>
-            <div className="absolute left-0 -bottom-1 w-0 h-[1px] bg-yellow-300 transition-all duration-300 group-hover:w-full"></div>
           </div>
-          <div className="relative group">
-            <h2 className="nav-links hover:text-gold-light cursor-pointer transition-all">
+          <div className="relative">
+            <h2 className="nav-links hover:text-gold-light cursor-pointer transition-all group">
               Sobre
+              <div className="absolute left-0 -bottom-1 w-0 h-[1px] bg-yellow-300 transition-all duration-300 group-hover:w-full"></div>
             </h2>
-            <div className="absolute left-0 -bottom-1 w-0 h-[1px] bg-yellow-300 transition-all duration-300 group-hover:w-full"></div>
           </div>
         </div>
       </div>
